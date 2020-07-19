376b8e66f960db28d3a0e212ccd9083b028ca9a3
injoon5 <52849281+injoon5@users.noreply.github.com>
2020-07-19 20:28:49 +0900

    Create  testing.js

---
+++ testing.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>자바스크립트 시작</title>
+<script>
+
+
+for (i = 0; i < 5; i++) {
+  // Runs 5 times, with values of step 0 through 4.
+  alert("hello "+i+" times. ");
+  
+}
+</script>
+</head>
+<body>
+</body>
+</html>
